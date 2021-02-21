e5e118aded0297887e08d06b58c25f77a110502d
Eric Lin <reaver@flomesh.io>
2021-02-21 22:50:46 +0800

    add sleuth dependency

--- reviews/src/main/java/com/redhat/bookinfo/reviews/ReviewsApplication.java
+++ reviews/src/main/java/com/redhat/bookinfo/reviews/ReviewsApplication.java
@@ -6,12 +6,10 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableCircuitBreaker
 @EnableHystrix
-@EnableHystrixDashboard
 @EnableFeignClients(basePackages = "com.redhat.bookinfo.ratings")
 @ComponentScan(basePackages = "com.redhat.bookinfo")
 @SpringBootApplication
