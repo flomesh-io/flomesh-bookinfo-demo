5e093d7b2c29e0bb5ab0f2664c2dedba0de1e308
Eric Lin <reaver@flomesh.io>
2021-02-21 21:36:59 +0800

    k8s deployment

--- config-service/src/main/java/com/redhat/bookinfo/config/ConfigServiceApplication.java
+++ config-service/src/main/java/com/redhat/bookinfo/config/ConfigServiceApplication.java
@@ -2,10 +2,12 @@ package com.redhat.bookinfo.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
