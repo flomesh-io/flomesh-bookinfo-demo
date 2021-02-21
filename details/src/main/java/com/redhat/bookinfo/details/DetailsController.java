99be51858a3e5ab6e3a11e69781907cec74e7c49
Eric Lin <reaver@flomesh.io>
2021-02-21 22:26:50 +0800

    fix reviews and ratings service URL issue

--- details/src/main/java/com/redhat/bookinfo/details/DetailsController.java
+++ details/src/main/java/com/redhat/bookinfo/details/DetailsController.java
@@ -1,13 +1,10 @@
 package com.redhat.bookinfo.details;
 
-import com.redhat.bookinfo.details.googlebooks.GoogleBooksVolumeInfoWrapper;
-import com.redhat.bookinfo.details.googlebooks.GoogleBooksWrapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class DetailsController {
