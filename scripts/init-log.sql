83fd52219b0d6cbd0f4f4e3b41121c8ed04f2559
Eric Lin <7455240+linyang0625@users.noreply.github.com>
2021-05-18 23:45:03 +0800

    update table DDL to latest version

--- scripts/init-log.sql
+++ scripts/init-log.sql
@@ -1,10 +1,20 @@
 CREATE TABLE default.log
 (
-    `uuid` String DEFAULT JSONExtractString(message, 'uuid'),
-    `type` String DEFAULT JSONExtractString(message, 'type'),
-    `message` String,
-    `timestamp` DateTime DEFAULT now()
+    `startTime` Int64 DEFAULT JSONExtractInt(message, 'startTime'),
+    `endTime` Int64 DEFAULT JSONExtractInt(message, 'endTime'),
+    `latency` Int64 DEFAULT JSONExtractInt(message, 'latency'),
+    `status` Int16 DEFAULT JSONExtractInt(response, 'status'),
+    `statusText` String DEFAULT JSONExtractString(response, 'statusText'),
+    `protocol` String DEFAULT JSONExtractString(message, 'protocol'),
+    `method` String DEFAULT JSONExtractString(message, 'method'),
+    `path` String DEFAULT JSONExtractString(message, 'path'),
+    `headers` String DEFAULT JSONExtractRaw(message, 'headers'),
+    `body` String DEFAULT JSONExtractString(message, 'body'),
+    `response` String DEFAULT JSONExtractRaw(message, 'response'),
+    `response.protocol` String DEFAULT JSONExtractString(response, 'protocol'),
+    `message` String
 )
 ENGINE = MergeTree
-ORDER BY uuid
+PARTITION BY (toYYYYMM(toDateTime(startTime / 1000)))
+ORDER BY (status, startTime)
 SETTINGS index_granularity = 8192;
