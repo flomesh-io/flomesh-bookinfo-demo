54cbaeebee8886ec51dde40426ac3e8fe2a621c4
Addo.Zhang <duwasai@gmail.com>
2021-12-17 16:48:09 +0800

    1. add feign interceptor to propagate upstream header 2. disable hystrix

---
+++ commons/src/main/java/io/flomesh/bookinfo/common/HeaderPropagatingInterceptor.java
@@ -0,0 +1,32 @@
+package io.flomesh.bookinfo.common;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+@Component
+public class HeaderPropagatingInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate template) {
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (null == requestAttributes) {
+            return;
+        }
+        HttpServletRequest request = requestAttributes.getRequest();
+        Enumeration<String> names = request.getHeaderNames();
+        while (names.hasMoreElements()) {
+            String name = names.nextElement();
+            String value = request.getHeader(name);
+            if (null != value) {
+                template.header(name, value);
+            }
+        }
+    }
+
+}
