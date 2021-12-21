bfcd1739f26771e736112ef9497cd06f6b840b7a
Lin Yang <reaver@flomesh.io>
2021-12-21 21:32:46 +0800

    rename env variable names
    Signed-off-by: Lin Yang <reaver@flomesh.io>

--- api-gateway/run.sh
+++ api-gateway/run.sh
@@ -3,7 +3,7 @@ java -javaagent:/opentelemetry-javaagent.jar \
   -Dotel.traces.exporter=logging \
   -Dotel.metrics.exporter=none \
   -Dotel.propagators=tracecontext,baggage,b3multi \
-  -Dotel.resource.attributes="service.name=${_pod_serviceFullName},service.version=${_pod_serviceVersion},service.namespace=${_pod_ns},service.instance.id=${_pod_UID}" \
+  -Dotel.resource.attributes="service.name=${_samples_pod_serviceFullName},service.version=${_samples_pod_serviceVersion},service.namespace=${_samples_pod_ns},service.instance.id=${_samples_pod_UID}" \
   -Dotel.javaagent.debug=false \
   -Dotel.traces.sampler=parentbased_always_on \
   -Djava.security.egd=file:/dev/./urandom \
