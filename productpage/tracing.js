e39d21140d1184ef2a0e358bfb7c80e530c2803f
Lin Yang <reaver@flomesh.io>
2021-12-21 19:26:51 +0800

    add B3Propagator for product page
    Signed-off-by: Lin Yang <reaver@flomesh.io>

--- productpage/tracing.js
+++ productpage/tracing.js
@@ -3,10 +3,12 @@
 // Require dependencies
 const opentelemetry = require("@opentelemetry/sdk-node");
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
+const { B3Propagator, B3InjectEncoding } = require('@opentelemetry/propagator-b3');
 
 const sdk = new opentelemetry.NodeSDK({
   traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
-  instrumentations: [getNodeAutoInstrumentations()]
+  instrumentations: [getNodeAutoInstrumentations()],
+  textMapPropagator: new B3Propagator({ injectEncoding: B3InjectEncoding.MULTI_HEADER })
 });
 
 sdk.start()
